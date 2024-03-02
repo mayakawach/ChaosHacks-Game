239fed71ce5e6533ca23b46fac54b70b4e80fcfc
gatory <rueikuanwei112233@gmail.com>
2024-03-02 11:24:19 -0800

    chagnes

--- cookingChaos.py
+++ cookingChaos.py
@@ -1,24 +1,57 @@
 import pygame
+import time
 
 pygame.init()
 
+# Set-up
 SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 200
+SCREEN_HEIGHT = 300
+pygame.display.set_caption("Cooking Chaos")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+bg = pygame.image.load("sky.jpg")
 
-pygame.display.set_caption
 
-screen = pygame.display.set_mode((CONST_WIDTH, CONST_HEIGHT))
-running = True
+class Food:
+  def __init__(food, item, name, price):
+    food.item = item
+    food.name = name
+    food.price = price
 
-def draw():
-    screen.fill(("red"))
+# Initializing Vegtable List
+tomato = pygame.Rect(100, 150, 20, 20)
+veg = [tomato]
+t = Food(tomato,)
+t = Food(tomato, "tomato", 1.00)
+
+def drawFood(veg) :
+    for i in veg :
+        pygame.draw.rect(screen, "red", i)
     pygame.display.update()
 
-while running: 
+def draw():
+    # screen.fill(("red"))
+    bg.blit(screen, (0,0))
+    drawFood(veg)
+    pygame.display.update()
+
+
+RUNNING = True
+
+frames = pygame.time.Clock()
+while RUNNING: 
+    frames.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
 
+        # mouse_click = pygame.mouse.get_pressed()
+        # if mouse_click[pygame.MOUSEBUTTONUP]:
+        #     print(mouse_click)
+
+        #     tomato.x += 5
+    
+        
+    
     draw()
 
 pygame.quit()
