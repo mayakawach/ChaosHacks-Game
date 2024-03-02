770a78b1bdba27f2bd3f78dcbad1b55eea1edecd
Peter Soava <thelingo56@gmail.com>
2024-03-02 14:40:38 -0800

    adjust how button checks for collision

--- buttonMenu.py
+++ buttonMenu.py
@@ -26,7 +26,7 @@ class Button ():
         if position[0] in range (self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             states.current_state = states.State.GAME                                           
     def changeColor(self,position):
-        if position[0] in range (self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+        if self.rect.collidepoint(position):
             self.text= setup.font.render(self.text_input, True, "green") #if hovering over 
         else:
             self.text = setup.font.render(self.text_input, True, "white")
@@ -40,7 +40,6 @@ def mainMenu():
     drawGameTitle()
     button.update()
     button.changeColor(pygame.mouse.get_pos())
-    
     handleMenuInput()
             
 def handleMenuInput():
@@ -52,21 +51,3 @@ def drawGameTitle() :
     setup.screen.fill((0,0,0))
     text = setup.font.render("COOKING CHAOS", True, (255, 255, 255))
     setup.screen.blit(text, (290, 25))
-         
-    #in main menu now, can make seperate buttons using the class above 
-
-    #PLAY_BUTTON = Button(image=pygame.image.load("assests/Play Rect.png"), pos=(640,250)
-    #QUIT_BUTTON = Button(image=pygame.image.load("assests/Quit Rect.png"), pos=(640,400)
-
-    #quit something else
-
-    #play function 
-    '''def play ():
-        pygame.display.set_caption("play")
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        SCREEN.fill("black")
-
-        PLAY_TEXT
-        PLAY_RECT
-        SCREEN.blitt'''
