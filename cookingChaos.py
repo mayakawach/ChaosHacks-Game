91839d945aeca051efbbe4b45793c32b12c75d2e
gatory <rueikuanwei112233@gmail.com>
2024-03-02 16:30:54 -0800

    insert images

--- cookingChaos.py
+++ cookingChaos.py
@@ -15,7 +15,7 @@ def draw():
             buttonMenu.mainMenu()
             
         case states.State.GAME:
-            dragableSquare.game()
+            gameLogic.game()
     
     pygame.display.update()
     
@@ -28,8 +28,7 @@ while states.RUNNING:
             break
         elif event.type == gameLogic.timer.timer_event and states.current_state == states.State.GAME:
             gameLogic.timer.counter -= 1
-            
-            gameLogic.timer.text = gameLogic.timer.font.render(str(gameLogic.timer.counter), True, (0, 128, 0))
+            gameLogic.timer.text = gameLogic.timer.font.render(str(gameLogic.timer.counter), True, ("red"))
             if gameLogic.timer.counter == 0:
                 states.RUNNING = False
 
